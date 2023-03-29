e8d2b27d8e574d71e4db2c43a630e03380cabdb4
Pietro Giordano <giopi87@gmail.com>
2023-03-29 22:25:36 +0200

    avanzamento milestone 5

---
+++ src/components/AppCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+      name: 'AppCard',
+      props: {
+            card: Object
+      }
+}
+</script>
+
+<template>
+      <img :src="card.image" class="card-img-top" alt="...">
+      <div class="card-body">
+            <h3 class="card-title">{{ card.title }}</h3>
+            <h5 class="card-text">{{ card.type.name }}</h5>
+            <p class="card-text">{{ card.decription }}</p>
+
+            <div v-if="card.technologies.lenght > 0">
+                  <ul class="list-group list-group-flush">
+                        <li v-for="tech in card.technologies" class="list-group-item">
+                              {{ tech.name }}
+                        </li>
+                  </ul>
+            </div>
+      </div>
+</template>
+
+<style lang="scss"></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios';
+import AppCard from './AppCard.vue';
 
 export default {
       name: 'AppMain',
+      components: {
+            AppCard,
+      },
       data() {
             return {
                   projects: []
@@ -12,15 +16,26 @@ export default {
             axios
                   .get('http://127.0.0.1:8000/api/projects')
                   .then(response => {
-                        console.log(response);
+                        console.log(response.data.projects.data);
+                        this.projects = response.data.projects.data;
                   });
       }
 }
 </script>
 
 <template>
-      <div>
+      <div class="container">
+            <div class="row mb-3">
+                  <div class="col">
+                        <h1>Boolfolio</h1>
+                  </div>
+            </div>
 
+            <div class="row">
+                  <div v-for="project in projects" class="card col-3" style="width: 18rem;">
+                        <AppCard :card="project" />
+                  </div>
+            </div>
       </div>
 </template>
 
